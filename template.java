b96020083c015e6f4ccdb18b088bb7b8c4b92d0d
EstelleYU <iamestelleyu@gmail.com>
2022-06-07 08:44:36 +0900

    Daily Coding Tue 07 JUN 2022

---
+++ template.java
@@ -0,0 +1,22 @@
+
+/* -----------------------------------------------
+Wed 01 JUN 2022
+Daily Coding from Coplit CodeStates
+07_convertListToObject
+- Convert 2D Array to HashMap. (value cannot be overwritten.)
+--------------------------------------------------
+example)
+
+String[][] arr1 = new String[][]{{"apple","red"}, {"kiwi","green"}, {"grape","purple"}};
+String[][] arr2 = new String[][]{{"apple","red"}, {"kiwi","green"}, {"apple","??"}};
+
+output1 =        {apple=red, kiwi=green, grape=purple}
+output2 =        {apple=red, kiwi=green}
+
+--------------------------------------------------*/
+
+public class template {
+    public static void main(String[] args) {
+
+    }
+}
